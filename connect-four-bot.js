58fbb9fb8b4878ee867b7cfd1f62a2be4497ceec
Justin Pincar <justinpincar@gmail.com>
2013-08-06 17:48:42 -0700

    Working bot

---
+++ connect-four-bot.js
@@ -0,0 +1,40 @@
+var io = require('socket.io-client')
+var socket = io.connect('http://localhost:3001');
+
+var idStr = "justin-bot";
+var alias = idStr;
+
+var tryMove = function(data) {
+  var action = data.action;
+  if (!action) {
+    return;
+  }
+
+  var actionPlayer = action.player;
+  if (!action.player) {
+    return;
+  }
+
+  if (actionPlayer.idStr == idStr) {
+    socket.emit('action', {
+      action: 'play',
+      column: Math.floor(Math.random() * 7)
+    });
+  }
+};
+
+socket.on('connect', function() {
+  socket.on('setState', tryMove);
+  socket.on('setGame', tryMove);
+
+  socket.emit("identify", {
+    alias: alias,
+    idStr: idStr
+  });
+  setTimeout(function() {
+    socket.emit("action", {
+      action: "sit"
+    });
+  }, 1000);
+});
+
